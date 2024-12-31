20c99182afd21533333a30e04099a6dce8ada4e4
bhavikdodda <bhavikdodda@gmail.com>
2024-12-31 18:09:30 +0530

    browser tab logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" className="dark">
       <head>
         <title>Top Trading Cycle Visualization!</title>
-        {/* You can include meta tags or other global styles here */}
+        <link rel="icon" href="/logodark2.png" type="image/png" />
       </head>
       <body className="bg-dark dark:bg-gray-900 text-black dark:text-dark">
         {children} {/* This is where your page content will be rendered */}
